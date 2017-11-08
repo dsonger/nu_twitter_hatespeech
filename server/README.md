582de309c114438368cc4966f930edc7e9ce8552
dnr2 <dnr2@cin.ufpe.br>
2017-11-08 14:08:58 -0600

    request tweet text from API

--- server/README.md
+++ server/README.md
@@ -1,5 +1,15 @@
 # Server
 
+The server will run continuously waiting for requests.
+
+Requests are expected to use POST method, with parameters with the format: 
+
+```
+{tweet_id: TWEET_ID}
+```
+
+Where TWEET_ID is a string of the tweet id, according to the tweet URL.
+
 ## Requirements:
 * Python 3.6.*
 * Keras 
@@ -7,26 +17,28 @@
 * Gensim
 * Numpy
 * Gevent
+* Twython
 
 ## How to run locally:
-Open the terminal and run the following command. This will bring up the server with the default options.
+Open the terminal and run the following command. Optional arguments in [].
 
 ```
-python server.py
+server.py [-p PORT_NUMBER] -k TWITTER_APP_KEY -t TWITTER_APP_SECRET [-s]
 ```
 
-## Usage:
+### Argument details:
 
 ```
-server.py [-p PORT_NUMBER] [-s]
-```
+-p --port-number PORT_NUMBER :
+	Port number to be used, by default uses 8080
 
-optional arguments:
+-s --serve-page : 
+	By default returns "403 Forbidden" on GET methods. 
+	If this is argument is set, will serve a simple html 
+	page to interact with server.
+	
+-k --app_key TWITTER_APP_KEY:
 
-```
--p --port-number : Port number to be used, by default uses 8080
--s --serve-page	PORT_NUMBER : By default returns "403 Forbidden" on GET
-	methods.  If this is argument is set, will
-	serve a simple html  page to interact with
-	server.
+-t --app_secret TWITTER_APP_SECRET:
+	
 ```
--- server/server.py
+++ server/server.py
@@ -6,10 +6,25 @@ import functools
 from urllib.parse import parse_qs
 from gevent.pywsgi import WSGIServer
 from lstm_classifier import TwitterHateClassifier
+from twython import Twython
+from twython.exceptions import TwythonError
 
 DEFAULT_PORT_NUMBER = 8080
 
-def application(env, start_response, classifier = None, serve_page = False, response_page = None):
+def error_response(start_response, status_code):
+    start_response(status_code, [("Content-Type", "text/html; charset=utf-8")])
+    return [bytes('<h1>%s</h1>' % status_code, "utf-8")]
+
+def get_twitter_crawler(app_key, app_secret):
+    app_key = 'inAzbLHc9OGYy2TJAyzvv8kVy'
+    app_secret = 'XWv544eJo1IFdQZvNrEazM6tknDCJnsHxprlLsC1vEH3iKMEnX'
+
+    twitter = Twython(app_key, app_secret, oauth_version=2)
+    access_token = twitter.obtain_access_token()
+    return Twython(app_key, access_token=access_token)
+    
+def application(env, start_response, classifier = None, serve_page = False, 
+        response_page = None, tweet_crawler = None):
     print("env = ", env)
 
     # the JS uses POST, so if it isn't POST, either return default page or 403 status.
@@ -18,38 +33,61 @@ def application(env, start_response, classifier = None, serve_page = False, resp
             start_response("200 OK", [("Content-Type", "text/html; charset=utf-8")])
             return [response_page]
         else:
-            start_response("403 Forbidden", [("Content-Type", "text/html; charset=utf-8")])
-            return [b'<h1>403 Forbidden</h1>']
+            return error_response(start_response, "403 Forbidden")
+    
+    # Try to get tweet data from Twitter API using requested tweet id.
+    try:
+        post_data = env["wsgi.input"].read().decode("utf-8")
+        print(post_data)
+        parsed = parse_qs(post_data)
+        print(parsed)
+        tweet_id = parsed["tweet_id"][0]
+        print(tweet_id)
+        tweet_data = tweet_crawler.show_status(id = tweet_id)        
+        print(tweet_data)
+    except TwythonError as err:
+        print(err)
+        if "429 (Too Many Requests)" in str(err):
+            return error_response(start_response, "429 Too Many Requests")
+        else:
+            return error_response(start_response, "404 Not Found")
+    except err:
+        print(err)
+        return error_response(start_response, "400 Bad Request")
 
-    post_data = env["wsgi.input"].read().decode("utf-8")
-    
-    # TODO: check if post data contains valid tweet id
-    if post_data is None:
-        start_response("400 Bad Request", [("Content-Type", "text/html; charset=utf-8")])
-        return [b'<h1>400 Bad Request</h1>']
-    
-    parsed = parse_qs(post_data)
-    # tweet_id = parsed["tweetid"]
-    tweet_text = parsed["tweet"][0]
+    prediction = classifier.predict(tweet_data)
+    response = '{"prediction": "%s"}' % prediction
+    if not prediction == "none":
+        # TODO: Generate URL from IBM NLU API
+        news_url = "http://news.google.com"
+        response = '{"prediction": "%s", "news_url": "%s"}' % (prediction, news_url)
+
+    print("\ntweet:\n", tweet_data)
+    print("\nresponse = ", response)
+
     start_response("200 OK", [("Content-Type", "text/html; charset=utf-8")])
-    prediction = classifier.predict({"text": tweet_text})
-    print("\ntweet text:\n", tweet_text)
-    print("\nprediction = ", prediction)
-    response = bytes('{"prediction": "%s"}' % prediction, "utf-8")
-    return [response]
+    return [bytes(response, "utf-8")]
 
 def run_server():
     # Parse arguments.
     parser = argparse.ArgumentParser(description='Server for running Twitter hate speech detection.')
     parser.add_argument('-p', '--port-number', default=DEFAULT_PORT_NUMBER)
+    parser.add_argument('-k', '--app-key', required=True)
+    parser.add_argument('-t', '--app-secret', required=True)
     parser.add_argument('-s', '--serve-page', action='store_true', default=False)
     args = parser.parse_args()
+    
     port_number = int(args.port_number)
+    app_key = args.app_key
+    app_secret = args.app_secret
     serve_page = args.serve_page
     response_page = None
     
     # Load classifier.
     classifier = TwitterHateClassifier()
+    
+    # Start Twython twitter crawler.
+    tweet_crawler = get_twitter_crawler(app_key, app_secret)
     
     # Load interactive response page.
     if serve_page:
@@ -61,7 +99,7 @@ def run_server():
     # Start server
     host = socket.gethostbyname(socket.gethostname())
     address = host, port_number
-    app = functools.partial(application, classifier = classifier, serve_page = serve_page, response_page = response_page)
+    app = functools.partial(application, classifier = classifier, serve_page = serve_page, response_page = response_page, tweet_crawler = tweet_crawler)
     server = WSGIServer(address, app)
     server.backlog = 256
     print('\n########################################\n')
